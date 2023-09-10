11ca3949bcfa7fc28cfcd4e25e6d63bbb3c25376
Aman Verma <amanverma03328@gmail.com>
2023-09-10 10:57:48 +0530

    feature: added tool-card ui

---
+++ tool_data.js
@@ -0,0 +1,16 @@
+let tool_data=[
+    {
+        "name": "dummy name",
+        "description": "dummy description here",
+        "url": "the dummy url here",
+        "rating": 4,
+        "image": "dummy image path"
+    },
+    {
+        "name": "dummy name 2",
+        "description": "dummy description here 2",
+        "url": "the dummy url here 2",
+        "rating": 5,
+        "image": "dummy image path"
+    },
+]
