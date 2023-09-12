e1f93291cfda22f9f69f57109141ed9b9ba47b05
Aman Verma <amanverma03328@gmail.com>
2023-09-12 23:17:40 +0530

    fix: added UI for tab, started code for search functionality, and adding product image dynamically

--- functionality.js
+++ functionality.js
@@ -1,36 +1,55 @@
 $(document).ready(function(){
+    let searchBar = $("#search-input");
     let cardParent = $("#card-parent");
     let cardsHtml = ""
+    
+    // initial data load
+    loadCardData();
 
-    toolData.forEach(function(individualTool){
-        cardsHtml+= '<div class="card m-2">'+
-                        '<div class="content">'+
-                            '<div class="img"><img src="https://unsplash.it/200/200"></div>'+
-                            '<div class="cardContent">'+
-                                '<div class="visible-details">'+
-                                    '<h3>'+individualTool.name+'</h3>'+
-                                    '<p class="tech-name">'+individualTool.techCategory+'</p>'+
-                                '</div>'+
-                                '<div class="hidden-details d-flex flex-column justify-content-center align-items-center">'+
-                                    '<div class="description-parent">'+
-                                        '<p class="description">'+individualTool.description+'</p>'+
+    searchBar.change(function(){
+        alert(searchBar.val());
+    });
+
+    function loadCardData(){
+        toolData.forEach(function(individualTool){
+            let tool_image;
+            
+            if(individualTool.image !== "default_image"){
+                tool_image = individualTool.image;
+            }else{
+                tool_image = "./assets/site_images/gear_image.svg";
+            }
+            
+            cardsHtml+= '<div class="card m-2">'+
+                            '<div class="content">'+
+                                '<div class="img d-flex justify-content-center align-items-center"><img src="'+tool_image+'" alt="..."></div>'+
+                                '<div class="cardContent">'+
+                                    '<div class="visible-details">'+
+                                        '<h3>'+individualTool.name+'</h3>'+
+                                        '<p class="tech-name">'+individualTool.techCategory+'</p>'+
                                     '</div>'+
-                                    '<div class="d-flex justify-content-between w-100">'+
-                                        '<div class="rating-parent d-flex align-items-center">'+
-                                            '<img src="./assets/site_images/star.svg" alt="...">'+
-                                            '<span class="mx-2">'+individualTool.rating+'</span>'+
+                                    '<div class="hidden-details d-flex flex-column justify-content-center align-items-center">'+
+                                        '<div class="description-parent">'+
+                                            '<p class="description">'+individualTool.description+'</p>'+
                                         '</div>'+
-                                        '<div class="visit-button">'+
-                                            '<a href="'+individualTool.url+'" target="_blank"><span class="mx-5">visit</span></a>'+
+                                        '<div class="d-flex justify-content-between w-100">'+
+                                            '<div class="rating-parent d-flex align-items-center">'+
+                                                '<img src="./assets/site_images/star.svg" alt="...">'+
+                                                '<span class="mx-2">'+individualTool.rating+'</span>'+
+                                            '</div>'+
+                                            '<div class="visit-button">'+
+                                                '<a href="'+individualTool.url+'" target="_blank"><span class="mx-5">visit</span></a>'+
+                                            '</div>'+
                                         '</div>'+
                                     '</div>'+
                                 '</div>'+
                             '</div>'+
-                        '</div>'+
-                    '</div>';
-    
-    });
-    cardParent.html(cardsHtml);
+                        '</div>';
+        
+        });
+        cardParent.html(cardsHtml);
+    }
+
 });
 
 
